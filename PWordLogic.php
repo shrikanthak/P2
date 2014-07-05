279c9f0bf4e87403b5cb05cf2af2bbe81f53184e
shrikanthak <shrikanth_ak@yahoo.com>
2014-07-05 00:26:49 -0400

    First commit P2

---
+++ PWordLogic.php
@@ -0,0 +1,69 @@
+<?php
+
+$password='';
+
+$ArrayWords=Array('donkey',
+	'excitement','philantropist','phosphate','fantabulous','bummer','cplusplus',
+	'futuristic','horrendous','condescension','frustration','hydrangya',
+	'androgynous','salubrious','sangria','byzantine','penitentiary','pastoral',
+	'fraternity','sorority','buckingham','methyldihydroxy'
+	);
+
+$ArraySymbols=Array('~','!','@','#','$','%','^','&','*','(',')','<','>','?',':',';');
+$errorMessage='';
+$upperLimit=10;
+
+$bPostFromSubmit=false;
+
+//echo print_r($_POST);
+
+foreach(array_keys($_POST) as $ControlName)
+{
+	if ($ControlName=='btnSubmit')
+	{
+		$bPostFromSubmit=true;
+	}
+}
+
+if ($bPostFromSubmit)
+{
+	if(is_numeric($_POST['txtNumberOfWords']) ? ($_POST['txtNumberOfWords']>0) && ($_POST['txtNumberOfWords']<=10) : false)
+	{
+		$countWords=count($ArrayWords);
+		$errorMessage='';
+		for($i=0;$i<$_POST['txtNumberOfWords'];$i++)
+		{
+			$newWord=$ArrayWords[rand(0,$countWords-1)];
+			while(!(strpos($password,$newWord)===false))
+			{
+				$newWord=$ArrayWords[rand(0,$countWords-1)];
+			}
+			if (rand(1,3)==3)
+			{
+				$password.=ucfirst($newWord).'-';	
+			}
+			else
+			{
+				$password.=$newWord.'-';
+			}
+			
+		}
+
+		if(isset($_POST['chkIncludeSymbol']))
+		{
+			$password.=$ArraySymbols[rand(0,count($ArraySymbols)-1)];
+		}
+		if(isset($_POST['chkIncludeNumber']))
+		{
+			$password.=rand(0,100);
+		}
+	}
+	else
+	{
+		$errorMessage="Please enter a numeric value between 1 and $upperLimit";
+	}
+}
+else
+{
+	$password='';$errorMessage='';
+}
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<meta charset='utf-8'>
+		
+		<title> Password Generator</title>
+
+		<?php require 'PWordLogic.php';?>
+
+		<link rel='stylesheet' type='text/css' href='css/style.css'>
+
+
+
+	</head>
+
+	<body>
+		<h1 style='font-weight:bold;text-align:center'>xkcd Password Generator</h1>
+		<div style='margin-left:auto;margin-right:auto'>
+			<?php if($password!=''):?>
+				<p class='password'><?=$password?></p><br>
+			<?php elseif($errorMessage != ''): ?>	
+				<p class='errormessage'><?=$errorMessage?></p><br>
+			<?php endif ?>
+		</div>
+		<form action='index.php' method='POST' class='formstyle'>
+			<b>Input Number of Words:</b><input type='text' name='txtNumberOfWords'> <b>Maximum of <?=$upperLimit?> Words</b><br>
+			
+			<div class = 'optionsdiv'>
+				<input type='checkbox' name='chkIncludeSymbol'><b>Include Symbol</b><br>
+				<input type='checkbox' name='chkIncludeNumber'><b>Include Number</b><br>
+			</div>
+			<input type='submit' name='btnSubmit'>
+		</form>
+		<p class='details'>
+			<a href='http://xkcd.com/936/'>xkcd password strength</a><br>
+		
+			<a href='http://xkcd.com/936/'>
+				<img class='comic' src='http://imgs.xkcd.com/comics/password_strength.png' alt='xkcd style passwords'>
+			</a>
+			<br>
+		</p>
+	</body>
+
+</html>
